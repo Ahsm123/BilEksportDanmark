efaeafe09e40970b4637f0731aefe19752262080
AndersBruunWinde <150430188+AndersBruunWinde@users.noreply.github.com>
2024-05-10 18:54:25 +0200

    Car & Copy classes

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,5 +1,116 @@
 package model;
 
-public class Car {
+import java.util.ArrayList;
 
+public class Car {
+	private int milage;
+	private String manufacturer;
+	private String model;
+	private String fuelType;
+	private double co2Emission;
+	private int power;
+	private double acceleration;
+	private int topSpeed;
+	private String gearType;
+	private int noOfGears;
+	private ArrayList<Copy> copies;
+	
+	public Car(int milage,String manufacturer,String model,String fuelType,double co2Emission,
+			int power,double acceleration,int topSpeed,String gearType,int noOfGears){
+	
+		this.milage=milage;
+		this.manufacturer=manufacturer;
+		this.model=model;
+		this.fuelType=fuelType;
+		this.co2Emission=co2Emission;
+		this.power=power;
+		this.acceleration=acceleration;
+		this.topSpeed=topSpeed;
+		this.gearType=gearType;
+		this.noOfGears=noOfGears;
+		this.copies= new ArrayList<>();
+}
+
+	public int getMilage() {
+		return milage;
+	}
+
+	public void setMilage(int milage) {
+		this.milage = milage;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getFuelType() {
+		return fuelType;
+	}
+
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+
+	public double getCo2Emission() {
+		return co2Emission;
+	}
+
+	public void setCo2Emission(double co2Emission) {
+		this.co2Emission = co2Emission;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public double getAcceleration() {
+		return acceleration;
+	}
+
+	public void setAcceleration(double acceleration) {
+		this.acceleration = acceleration;
+	}
+
+	public int getTopSpeed() {
+		return topSpeed;
+	}
+
+	public void setTopSpeed(int topSpeed) {
+		this.topSpeed = topSpeed;
+	}
+
+	public String getGearType() {
+		return gearType;
+	}
+
+	public void setGearType(String gearType) {
+		this.gearType = gearType;
+	}
+
+	public int getNoOfGears() {
+		return noOfGears;
+	}
+
+	public void setNoOfGears(int noOfGears) {
+		this.noOfGears = noOfGears;
+	}
+	
+	
+	
 }
--- src/model/Copy.java
+++ src/model/Copy.java
@@ -1,5 +1,93 @@
 package model;
 
 public class Copy {
+	private String vin;
+	private double price;
+	private String state;
+	private String modification;
+	private int kilometer;
+	private int color;
+	private boolean taxReturn;
+	private boolean isInspected;
+	
+	public Copy(String vin,double price,String state,String modification,
+			int kilometer,int color,boolean taxReturn,boolean isInspected) {
+		
+		this.vin=vin;
+		this.price=price;
+		this.state=state;
+		this.modification=modification;
+		this.kilometer=kilometer;
+		this.color=color;
+		this.taxReturn=taxReturn;
+		this.isInspected=isInspected;
+		
+	}
+
+	public String getVin() {
+		return vin;
+	}
+
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getModification() {
+		return modification;
+	}
+
+	public void setModification(String modification) {
+		this.modification = modification;
+	}
+
+	public int getKilometer() {
+		return kilometer;
+	}
+
+	public void setKilometer(int kilometer) {
+		this.kilometer = kilometer;
+	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}
+
+	public boolean isTaxReturn() {
+		return taxReturn;
+	}
+
+	public void setTaxReturn(boolean taxReturn) {
+		this.taxReturn = taxReturn;
+	}
+
+	public boolean isInspected() {
+		return isInspected;
+	}
+
+	public void setInspected(boolean isInspected) {
+		this.isInspected = isInspected;
+	}
+	
+	
 
 }
