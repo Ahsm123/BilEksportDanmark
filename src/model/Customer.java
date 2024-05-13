28c5a897c2164b9ec307a9ec5b20ece827660ab9
ThomasUCN <10503119@ucn.dk>
2024-05-13 09:00:15 +0200

    Person, customer samt employee model klasser

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,40 @@
 package model;
 
-public class Customer {
+public class Customer extends Person{
+	public int cvr;
+	public int cpr;
+	public String adress;
+
+	public Customer(String Name, String phoneNo, int age, String email) {
+		super(Name, phoneNo, age, email);
+		this.cvr = cvr;
+		this.cpr = cpr;
+		this.adress = adress;
+	}
+
+	public int getCvr() {
+		return cvr;
+	}
+
+	public void setCvr(int cvr) {
+		this.cvr = cvr;
+	}
+
+	public int getCpr() {
+		return cpr;
+	}
+
+	public void setCpr(int cpr) {
+		this.cpr = cpr;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
 
 }
