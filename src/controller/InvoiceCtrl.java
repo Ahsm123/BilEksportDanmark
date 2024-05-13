6b19265a220ea7ee6f8e9a6c06f7a164870a0a25
ThomasUCN <10503119@ucn.dk>
2024-05-13 11:12:32 +0200

    invoicectrl

--- src/controller/InvoiceCtrl.java
+++ src/controller/InvoiceCtrl.java
@@ -1,5 +1,28 @@
 package controller;
 
+import java.sql.SQLException;
+
+import model.Invoice;
+import model.database.DataAccessException;
+import model.database.InvoiceDB;
+
 public class InvoiceCtrl {
 
+	private InvoiceDB invoiceDB;
+
+	public InvoiceCtrl()  {
+		try {
+			invoiceDB = new InvoiceDB();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+
+	public void saveInvoiceInDB() throws SQLException, DataAccessException {
+		 invoiceDB.saveInvoiceInDB();	
+
+	}
 }
