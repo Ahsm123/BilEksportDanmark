1c1f4d0dc83f2acd02b7bdf1cee2317aab9b4e3c
ThomasUCN <10503119@ucn.dk>
2024-05-13 09:10:01 +0200

    ændring af constructor

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,5 +1,32 @@
 package model;
 
-public class Employee {
+public class Employee extends Person {
+	public int id;
+	public double salary;
+
+	public Employee(String name, String phoneNo, int age, String email) {
+		super(name, phoneNo, age, email);
+		this.id = id;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+	
 
 }
