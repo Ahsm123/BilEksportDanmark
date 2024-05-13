98fc958ee3be2d3f96713a26ad8160f05d7e1cec
Andershsm <10503096@ucn.dk>
2024-05-13 11:17:22 +0200

    Update OrderCtrl.java

--- src/controller/OrderCtrl.java
+++ src/controller/OrderCtrl.java
@@ -1,49 +1,59 @@
 package controller;
+
+import model.Customer;
+import model.Employee;
 import model.Order;
+import model.database.OrderDB;
 import model.database.OrderDBIF;
 import java.sql.SQLException;
 
 public class OrderCtrl {
 	private OrderDBIF orderDB;
-	private Order currentORder;
 	private CustomerCtrl customerCtrl;
-//	private CopyCtrl copyCtrl;
-//	private InvoiceCtrl invoiceCtrl;
-//	
-//	public OrderCtrl() throws DataAccessException {
-//		this.orderDB = new OrderDB();
-//		this.customerCtrl = new CustomerCtrl;
-//		this.productCtrl = new ProductCtrl;
-//		this.invoiceCtrl = new InvoiceCtrl;
+	private CarCtrl carCtrl;
+	private InvoiceCtrl invoiceCtrl;
+	private Order currentOrder;
+	private Customer customer;
+	private Employee employee;
+
+	public OrderCtrl() throws DataAccessException {
+	
+		this.orderDB = new OrderDB();
+		this.customerCtrl = new CustomerCtrl();
+		this.carCtrl = new CarCtrl();
+		this.invoiceCtrl = new InvoiceCtrl();
 	}
 
-	private Customer findCustomer(String phoneNo) throws new DataAccessException {
+	private Customer findCustomer(String phoneNo) throws DataAccessException {
 		return customerCtrl.findCustomer(phoneNo);
-
-	private Order createOrder(String phoneNo) throws DataAccessException {
-		Customer customer = findCustomer(phoneNo);
 		
+	}
+
+	private Order createOrder(String phoneNo) throws NullPointerException {
+		Customer customer = findCustomer(phoneNo);
+
 		if (customer != null) {
-			currentOrder = new Order(customer);
+			currentOrder = new Order(employee, customer);
 		} else {
 			throw new NullPointerException("No customer found");
 		}
 		return currentOrder;
 	}
-	
-	private findCopy(String vin) throws DataAccessException {
-		return copyCtrl.findCopy(vin);
+
+	private Copy findCopy(String vin) throws DataAccessException {
+		return carCtrl.findCopy(vin);
 	}
-	
+
 	public void addCopy(String vin) throws DataAccessException {
 		Copy copy = findCopy(vin);
-		
-		if(copy != null) {
+
+		if (copy != null) {
 			currentOrder.addCopy(copy);
 		}
-		
+
 	}
+
 	public void confirmOrder() throws SQLException {
-		orderDB.saveOrder(currentOrder) && invoiceController.saveInvoiceInDB(invoiceCtrl.createInvoice(currentOrder));
+		
 	}
 }
