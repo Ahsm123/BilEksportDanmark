ad3bb8191d2cab7c67324d2c219ef96939015bf5
AndersBruunWinde <10503107@ucn.dk>
2024-05-14 11:24:04 +0200

    CarCtrl findCopy metode

--- src/controller/CarCtrl.java
+++ src/controller/CarCtrl.java
@@ -1,16 +1,27 @@
 package controller;
 
 import model.Copy;
+import model.database.CarDB;
 import model.database.CarDBIF;
+import model.database.DataAccessException;
 
 public class CarCtrl {
 
 	private CarDBIF carDB;
 	
-	public CarCtrl() {}
+	public CarCtrl() {
+		try { 
+			this.carDB = new CarDB();
+	}catch (DataAccessException e) {
+		
+		e.printStackTrace();
+	}
 	
-	public Copy findCopy(String vin) {
-		return null;
+		
+	}
+	
+	public Copy findCopy(String vin) throws DataAccessException{
+		return carDB.findCopy(vin);
 		
 	}
 	
