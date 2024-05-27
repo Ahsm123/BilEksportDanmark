0a6fff880f1206d47151d457e8b71902bbd5d3e4
Andershsm <10503096@ucn.dk>
2024-05-27 11:23:58 +0200

    OrderCtrlIntegrationsTest

--- src/test/OrderCtrlIntegrationTest.java
+++ src/test/OrderCtrlIntegrationTest.java
@@ -56,7 +56,7 @@ public class OrderCtrlIntegrationTest {
 	private void setUpDatabase() throws SQLException {
 		try (Statement stmt = connection.createStatement()) {
 			stmt.execute("delete from order");
-			stmt.execute("delete from customer");
+
 
 		}
 	}
@@ -69,7 +69,7 @@ public class OrderCtrlIntegrationTest {
 	private void tearDownDatabase() throws SQLException {
 		try (Statement stmt = connection.createStatement()) {
 			stmt.execute("delete from order");
-			stmt.execute("delete from customer");
+	
 		}
 	}
 
